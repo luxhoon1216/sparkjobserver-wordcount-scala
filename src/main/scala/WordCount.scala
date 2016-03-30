c71bd0de4dd893427780218d5f6f82b79528b496
luxhoon1216 <luxhoon1216@gmail.com>
2016-03-29 21:48:42 -0500

    initial upload

--- src/main/scala/WordCount.scala
+++ src/main/scala/WordCount.scala
@@ -1,17 +1,27 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
+import spark.jobserver.SparkJob
+import spark.jobserver.SparkJobValidation
+import com.typesafe.config.Config
+import spark.jobserver.SparkJobInvalid
+import spark.jobserver.SparkJobValid
+import scala.util.Try
 
-object WordCount {
-  def main(args: Array[String]) {
-    val sc = new SparkContext(new SparkConf().setAppName("Spark Word Count").setMaster(args(0)))
- 
-    // split each document into words
-    val tokenized = sc.textFile(args(1)).flatMap(_.split(" "))
+
+object WordCount extends SparkJob {
+  
+  override def validate(sc: SparkContext, config: Config): SparkJobValidation = {
+    Try(config.getString("inputFile"))
+    .map(x => SparkJobValid)
+    .getOrElse(SparkJobInvalid("No inputFile"))
+  }
+  
+  override def runJob(sc: SparkContext, config: Config): Any = {
+    val tokenized = sc.textFile(config.getString("inputFile")).flatMap(_.split(" "))
  
     // count the occurrence of each word
     val wordCounts = tokenized.map((_, 1)).reduceByKey(_ + _)
- 
-    System.out.println(wordCounts.collect().mkString(", "))
+    wordCounts.collect().toMap
   }
 }
